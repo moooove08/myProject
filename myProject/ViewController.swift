b0d19cf9e4441a7e33b27fb07026b18751a9ec2a
moooove08 <tj.vlad@gmail.com>
2023-09-18 12:30:59 +0300

    Start project

---
+++ myProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myProject
+//
+//  Created by Vlad Kuzmenko on 18.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
